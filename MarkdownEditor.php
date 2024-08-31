a6033496a2227596d16abbbb00bd1461a9b81d12
Antonín Kazda <xkazda1@fi.muni.cz>
2024-08-31 15:42:16 +0200

    feat: add image upload

--- MarkdownEditor.php
+++ MarkdownEditor.php
@@ -51,32 +51,29 @@ class MarkdownEditor extends InputWidget
         $view->registerJs($script);
 
         if ($this->uploadUrl !== null) {
+            $csrfParam = \Yii::$app->request->csrfParam;
+            $csrfToken = \Yii::$app->request->csrfToken;
             MarkdownEditorUploadAsset::register($view);
             $view->registerJs("
                 inlineAttachment.editors.codemirror4.attach($varName.codemirror, {
                     onFileUploadResponse: function(xhr) {
                         var result = JSON.parse(xhr.responseText);
-                        console.log(result);
                         filename = result[this.settings.jsonFieldName];
-                        console.log(filename);
-                        console.log(this.filenameTag);
                         if (result && filename) {
                             var newValue;
-                            if (typeof this.settings.urlText === 'function') {
-                                newValue = this.settings.urlText.call(this, filename, result);
-                            } else {
-                                newValue = this.settings.urlText.replace(this.filenameTag, filename);
-                            }
-                            console.log(newValue);
+                            newValue = this.settings.urlText.replace(this.filenameTag, filename);
                             var text = this.editor.getValue().replace(this.lastValue, newValue);
                             this.editor.setValue(text);
                             this.settings.onFileUploaded.call(this, filename);
                         }
                         return false;
                     },
+                    extraParams: {
+                        $csrfParam: '$csrfToken'
+                    },
                     uploadUrl: '" . $this->uploadUrl . "',
                     jsonFieldName: 'filename',
-                    urlText: \"![Image]({filename})\"
+                    urlText: \"![" . \Yii::t('app', 'Image description') . "]({filename})\"
                 });
             ");
         }
@@ -91,6 +88,54 @@ class MarkdownEditor extends InputWidget
     {
         $this->editorOptions['element'] = new JsExpression('document.getElementById("' . $this->options['id'] . '")');
 
+        $this->editorOptions['toolbar'] = $this->getToolbarConfig();
+
         return Json::encode($this->editorOptions);
+    }
+
+    protected function getToolbarConfig() {
+        $toolbar =  [
+            'bold', 'italic', 'heading-1', 'heading-2', 'heading-3', '|',
+            'code', 'unordered-list', 'ordered-list', '|',
+            'link', 'image', 
+        ];
+        
+        if($this->uploadUrl !== null) {
+            $toolbar[] = [
+                'name' => 'image-upload',
+                'action' => new JsExpression('function customFunction(editor){
+                    let inputId = "editor-image-upload-input";
+                    if(document.getElementById(inputId) === null){
+                        let input = document.createElement("input");
+                        input.type = "file";
+                        input.accept = "image/*";
+                        input.style.display = "none";
+                        input.id = "editor-image-upload-input";
+                        input.onchange = function(){
+                            let file = this.files[0];
+                            let formData = new FormData();
+                            formData.append("file", file);
+                            formData.append("' . \Yii::$app->request->csrfParam . '", "' . \Yii::$app->request->getCsrfToken() . '");
+                            fetch("' . $this->uploadUrl . '", {
+                                method: "POST",
+                                body: formData
+                            }).then(response => response.json()).then(data => {
+                                editor.codemirror.replaceSelection("![' . \Yii::t('app', 'Image description') . '](" + data.filename + ")");
+                            });
+                        }
+                        document.body.appendChild(input);
+                    }
+                    document.getElementById(inputId).click();
+
+                }'),
+                'className' => 'fa fa-file-image',
+                'title' => 'Upload image',
+            ];
+        }
+        $toolbar = array_merge($toolbar, [
+            'table', 'horizontal-rule', '|',
+            'preview', 'fullscreen'
+        ]);
+        return $toolbar;
     }
 }
